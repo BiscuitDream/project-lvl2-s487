ece850737c980daadce42c70f5fa5306f6042fb7
Igor Pechenkin <ipechenkin@gmail.com>
2019-07-29 01:45:25 +0300

    add commander, add --help

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 // https://github.com/evanw/node-source-map-support пакет для поддержки нодой node source map
+
+import program from 'commander';
+
+program
+  .version('0.0.1')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format');
+
+program.parse(process.argv);
+
 const gendiff = () => {
-  console.log('Test!!! Hello!!');
+  console.log('It\'s work!!');
 };
 
 export default gendiff;
