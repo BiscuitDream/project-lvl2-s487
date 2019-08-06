ab15bd9a3567e45cc21187b8c05922517e94708d
Igor Pechenkin <ipechenkin@gmail.com>
2019-08-07 02:14:54 +0300

    begin genDiff function for json

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 // https://github.com/evanw/node-source-map-support пакет для поддержки нодой node source map
 
-// import program from 'commander';
-
-// program
-//   .version('0.0.1')
-//   .description('Compares two configuration files and shows a difference.')
-//   .arguments('<firstConfig> <secondConfig>')
-//   .option('-f, --format [type]', 'Output format')
-//   .parse(process.argv);
-
-
-// if (!program.args.length) program.help();
-
-// const gendiff = () => {
-//   console.log('It\'s work!!');
-// };
-
-// export default gendiff;
-
-
-// const genDiff = (filepath1, filepath2) => {
-//   const fileData1 = JSON.parse(fs.readFileSync(filepath1), 'utf-8');
-//   const fileData2 = JSON.parse(fs.readFileSync(filepath2), 'utf-8');
-//   return getDiff(fileData1, fileData2);
-// };
-
-// export default genDiff;
-
 
 // import genDiff from 'genDiff';
 // const diff = genDiff(pathToFile1, pathToFile2);
@@ -34,6 +7,37 @@
 
 
 import fs from 'fs';
+// import _ from 'lodash';
+
+const getDiff = (file1, file2) => {
+  const file1Keys = Object.keys(file1);
+  const file2Keys = Object.keys(file2);
+
+  const diff1 = file1Keys.reduce((acc, key) => {
+    if (file2Keys.includes(key)) {
+      if (file1[key] === file2[key]) {
+        return `${acc}    ${key}: ${file1[key]}\n`;
+      }
+      return `${acc}  + ${key}: ${file2[key]}\n  - ${key}: ${file1[key]}\n`;
+    }
+    return `${acc}  - ${key}: ${file1[key]}\n`;
+  }, '');
+
+  const diff2 = file2Keys.reduce((acc, key) => {
+    if (!file1Keys.includes(key)) {
+      return `${acc}  + ${key}: ${file2[key]}`;
+    }
+    return acc;
+  }, diff1);
+
+  const diff = `{\n${diff2}\n}`;
+
+  // console.log(diff1);
+  // console.log(diff2);
+  console.log(diff);
+
+  return diff;
+};
 
 const genDiff = (pathToFile1, pathToFile2) => {
   const file1 = fs.readFileSync(pathToFile1, 'utf-8');
@@ -42,22 +46,37 @@ const genDiff = (pathToFile1, pathToFile2) => {
   const file2 = fs.readFileSync(pathToFile2, 'utf-8');
   const parsedFile2 = JSON.parse(file2);
 
-  console.log('file1 :', file1);
-  console.log('typeof file1:', typeof file1);
-  console.log();
-  console.log('parsedFile1 :', parsedFile1);
-  console.log('typeof parsedFile1 :', typeof parsedFile1);
-  console.log();
-  console.log('====================');
+  return getDiff(parsedFile1, parsedFile2);
 
-  console.log('file2 :', file2);
-  console.log('typeof file2:', typeof file2);
-  console.log();
-  console.log('parsedFile2 :', parsedFile2);
-  console.log('typeof parsedFile2 :', typeof parsedFile2);
-  // fs.readFile('/etc/passwd', 'utf8', callback);
+  // console.log('file1 :', file1);
+  // console.log('typeof file1:', typeof file1);
+  // console.log();
+  // console.log('parsedFile1 :', parsedFile1);
+  // console.log('typeof parsedFile1 :', typeof parsedFile1);
+  // console.log();
+  // console.log('====================');
+
+  // console.log('file2 :', file2);
+  // console.log('typeof file2:', typeof file2);
+  // console.log();
+  // console.log('parsedFile2 :', parsedFile2);
+  // console.log('typeof parsedFile2 :', typeof parsedFile2);
 };
 
 export default genDiff;
 
-// сделать, чтобы файлы читались из всех мест. чтобы можно было указывать относительные пути
+// Правильная проверка существования ключа в объекте - функция has из lodash.
+// var object = { 'a': { 'b': 2 } };
+// var other = _.create({ 'a': _.create({ 'b': 2 }) });
+
+// _.has(object, 'a');
+// // => true
+
+// _.has(object, 'a.b');
+// // => true
+
+// _.has(object, ['a', 'b']);
+// // => true
+
+// _.has(other, 'a');
+// // => false
