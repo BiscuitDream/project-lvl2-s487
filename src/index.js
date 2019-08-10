56392e9a2681977e681a4e9ff16f5221d368a26e
Igor Pechenkin <ipechenkin@gmail.com>
2019-08-11 01:51:36 +0300

    del comments

--- src/index.js
+++ src/index.js
@@ -9,10 +9,6 @@ const getDiff = (data1, data2) => {
   const data2Keys = Object.keys(data2);
   const dataKeys = [...(new Set(data1Keys.concat(data2Keys)))];
 
-  // console.log('data1Keys :', data1Keys);
-  // console.log('data2Keys :', data2Keys);
-  // console.log('dataKeys :', dataKeys);
-
   const rawDiff = dataKeys.reduce((acc, key) => {
     if (data1Keys.includes(key) && data2Keys.includes(key)) {
       if (data1[key] === data2[key]) {
@@ -25,36 +21,8 @@ const getDiff = (data1, data2) => {
     }
     return `${acc}  - ${key}: ${data1[key]}\n`;
   }, '');
-  // console.log(rawDiff);
 
   const diff = `{\n${rawDiff}}`;
-
-  // const testKeys = _.union(file1Keys, file2Keys);
-  // console.log('testKeys :', testKeys);
-
-  // const diff1 = data1Keys.reduce((acc, key) => {
-  //   if (data2Keys.includes(key)) {
-  //     if (data1[key] === data2[key]) {
-  //       return `${acc}    ${key}: ${data1[key]}\n`;
-  //     }
-  //     return `${acc}  + ${key}: ${data2[key]}\n  - ${key}: ${data1[key]}\n`;
-  //   }
-  //   return `${acc}  - ${key}: ${data1[key]}\n`;
-  // }, '');
-
-  // const diff2 = data2Keys.reduce((acc, key) => {
-  //   if (!data1Keys.includes(key)) {
-  //     return `${acc}  + ${key}: ${data2[key]}\n`;
-  //   }
-  //   return acc;
-  // }, diff1);
-
-  // console.log(diff1);
-  // console.log(diff2);
-  // console.log(diff);
-
-
-  // const diff = `{\n${diff2}}`;
 
   return diff;
 };
@@ -70,19 +38,3 @@ const genDiff = (file1Path, file2Path) => {
 };
 
 export default genDiff;
-
-// Правильная проверка существования ключа в объекте - функция has из lodash.
-// var object = { 'a': { 'b': 2 } };
-// var other = _.create({ 'a': _.create({ 'b': 2 }) });
-
-// _.has(object, 'a');
-// // => true
-
-// _.has(object, 'a.b');
-// // => true
-
-// _.has(object, ['a', 'b']);
-// // => true
-
-// _.has(other, 'a');
-// // => false
