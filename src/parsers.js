b99b428f3331bb21ad71f99211c6415a9a4fe899
Igor Pechenkin <ipechenkin@gmail.com>
2019-08-13 14:06:52 +0300

    change choice of parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,25 +1,25 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const getParse = (extension) => {
-  if (extension === '.json') {
-    return JSON.parse;
-  }
-  if (extension === '.ini') {
-    return ini.parse;
-  }
-  return yaml.safeLoad;
-};
+// const getParse = (extension) => {
+//   if (extension === '.json') {
+//     return JSON.parse;
+//   }
+//   if (extension === '.ini') {
+//     return ini.parse;
+//   }
+//   return yaml.safeLoad;
+// };
 
 // Полиморфизм на основе объекта
 // черновик
-// const parsers = {
-//   '.json': JSON.parse,
-//   '.yaml': yaml.safeLoad,
-//   '.yml': yaml.safeLoad,
-//   '.ini': ini.parse,
-// };
+const parsers = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad,
+  '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
+};
 
-// const getParse = extension => parsers[extension];
+const getParse = extension => parsers[extension];
 
 export default getParse;
