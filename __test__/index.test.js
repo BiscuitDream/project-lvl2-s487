eb2aae52c685e33bc27d563ea07a3c9053367725
Igor Pechenkin <ipechenkin@gmail.com>
2019-08-14 23:22:23 +0300

    delete comments

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,37 +1,5 @@
 import fs from 'fs';
 import genDiff from '../src';
-// npx jest --coverage
-
-// test('genDiff flat json', () => {
-//   // const resultPath = path.resolve(__dirname, './__fixtures__/result-json.txt');
-//   const resultPath = `${__dirname}/__fixtures__/result-json.txt`;
-//   const result = fs.readFileSync(resultPath, 'utf-8').trim();
-
-//   const beforePath = `${__dirname}/__fixtures__/before.json`;
-//   const afterPath = `${__dirname}/__fixtures__/after.json`;
-
-//   expect(genDiff(beforePath, afterPath)).toEqual(result);
-// });
-
-// test('genDiff flat yaml', () => {
-//   const resultPath = `${__dirname}/__fixtures__/result-yaml.txt`;
-//   const result = fs.readFileSync(resultPath, 'utf-8').trim();
-
-//   const beforePath = `${__dirname}/__fixtures__/before.yml`;
-//   const afterPath = `${__dirname}/__fixtures__/after.yml`;
-
-//   expect(genDiff(beforePath, afterPath)).toEqual(result);
-// });
-
-// test('genDiff flat ini', () => {
-//   const resultPath = `${__dirname}/__fixtures__/result-ini.txt`;
-//   const result = fs.readFileSync(resultPath, 'utf-8').trim();
-
-//   const beforePath = `${__dirname}/__fixtures__/before.ini`;
-//   const afterPath = `${__dirname}/__fixtures__/after.ini`;
-
-//   expect(genDiff(beforePath, afterPath)).toEqual(result);
-// });
 
 const filesForTest = [
   ['before.json', 'after.json', 'expected-flat.txt'],
--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,6 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-// const getParse = (extension) => {
-//   if (extension === '.json') {
-//     return JSON.parse;
-//   }
-//   if (extension === '.ini') {
-//     return ini.parse;
-//   }
-//   return yaml.safeLoad;
-// };
-
-// Полиморфизм на основе объекта
-// черновик
 const parsers = {
   '.json': JSON.parse,
   '.yaml': yaml.safeLoad,
