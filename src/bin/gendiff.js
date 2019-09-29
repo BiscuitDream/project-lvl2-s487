7096835ac3f46cf764fd44c9c9829a67d7422437
Igor Pechenkin <ipechenkin@gmail.com>
2019-09-30 00:29:07 +0300

    fix default format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,7 +5,7 @@ import genDiff from '..';
 program
   .version('0.0.3')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format', 'common')
+  .option('-f, --format [type]', 'Output format', 'nested')
   .arguments('<firstConfig> <secondConfig>')
   .action((file1, file2) => console.log(genDiff(file1, file2, program.format)))
   .parse(process.argv);
