4a76d8343c74609b33d6c8d7dc41b76c7f5430c6
Igor Pechenkin <ipechenkin@gmail.com>
2019-10-02 02:44:28 +0300

    string to array v2

--- src/formatters/plainFormatter.js
+++ src/formatters/plainFormatter.js
@@ -16,11 +16,11 @@ const getValue = (value) => {
 const plainFormatter = (ast) => {
   const iter = (elem, name) => {
     if (elem instanceof Array) {
-      let string = '';
-      for (let i = 0; i < elem.length; i += 1) {
-        string = `${string}${(elem[i].type === 'unchanged' ? '' : `${iter(elem[i], name)}\n`)}`;
-      }
-      return string.trim();
+      const strings = elem
+        .filter(el => el.type !== 'unchanged')
+        .map(el => iter(el, name));
+
+      return strings.join('\n');
     }
 
     if (elem.kind === 'parametre') {
